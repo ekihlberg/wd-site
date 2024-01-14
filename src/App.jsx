80042ab98555fcf12bfb0ec0abb99f2f3ca4a83f
Emma Kihlberg <emmakihlberg@Emmas-MBP.home>
2024-01-14 14:10:20 +0100

    top part changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
 
   return (
     <>
-      <div>
-      </div>
+      <div className='top_wrapper'>
+      
       <p className="top-header-subtext">
        21 september 2024
       </p>
       <h1 className="top-header-text">Emma <br/>Tobias</h1>
-     
+      </div>
     </>
   )
 }
