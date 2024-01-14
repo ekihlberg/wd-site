bdac2e6de705e9369d32d393103e22928456d3ea
Emma Kihlberg <emmakihlberg@Emmas-MBP.home>
2024-01-14 15:23:50 +0100

    text section 1

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,13 @@ function App() {
       </p>
       <h1 className="top-header-text">Emma <br/>Tobias</h1>
       </div>
+      <div className='section section_info section_info__dark'>
+      <div className='section_text'> 
+        <h3>VÅR SAGA</h3>
+        <h2>Njuta, Äta , Mat....</h2>
+        <p>Genom en gemensam passion för mat och upplevelser, som började med en matchning på en dejtingapp, formades en djup vänskap. Våra middagsbjudningar blev snabbt en central del av vårt umgänge, där vi turades om att skapa en varm och välkomnande atmosfär hemma hos varandra. Kvällarna var fyllda med skratt, läckra rätter och en känsla av samhörighet. I våra kök spirade en vänskap, kryddad med kärleken till matlagning och det glada sällskapet.</p>
+        </div>
+      </div>
     </>
   )
 }
