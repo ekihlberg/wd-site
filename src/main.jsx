39e570e3847eee072868284f177749628d9f7ed4
Emma Kihlberg <emmakihlberg@FVFH709gitalcom.localdomain>
2024-11-05 16:15:11 +0100

    redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CustomRoutes from './Routes.jsx'; // Import the Routes component
 import Grid from './grid.jsx'
 import App from './App.jsx'
 import FunFacts from './funfacts.jsx'
@@ -13,7 +12,6 @@ import WeddingRSVPForm from './WeddingRSVPForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CustomRoutes>
       <Grid />
       <App />
       <FunFacts />
@@ -21,6 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Mapfunc />
       <Faq />
       <Vigsel />
-    </CustomRoutes>
   </React.StrictMode>
 );
